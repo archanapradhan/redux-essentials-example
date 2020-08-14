bb4dc34cb17c35d16a512740bd7a6c845fd5b3dc
Archana Pradhan <mail.archanapradhan@gmail.com>
2020-08-15 00:05:26 +1200

    created a reducer postsSlice and added the reducer to store

---
+++ src/features/posts/postsSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const postsSlice = createSlice({
+  name: 'posts',
+  initialState: [
+    { id: '1', title: 'First Post!', content: 'Hello!' },
+    { id: '2', title: 'Second Post!', content: 'More text' },
+  ],
+  reducers: {},
+})
+
+export default postsSlice.reducer
